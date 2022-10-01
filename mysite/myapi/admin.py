ee9876d8ac1d419fd59a87674b70b049facf9900
alexmorosan <alex.morosan@gmail.com>
2022-10-01 16:49:17 -0400

    created book and genre get api

--- mysite/myapi/admin.py
+++ mysite/myapi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 from .models import Wishlists
+from .models import Books
+from .models import Genres
 admin.site.register(Wishlists)
+admin.site.register(Books)
+admin.site.register(Genres)
