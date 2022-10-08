2a099c7fffb7362b66f0851d0a36c37f9e9c0e32
stephano montes <112264544+stephanomontes@users.noreply.github.com>
2022-10-07 22:12:31 -0400

    Revert "Add files via upload (#21)"
    This reverts commit c7749b6caf6b7ffb07316da10fa85eafa5ecd5cb.

--- mysite/mysite/myapi/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Wishlists
-from .models import Books
-from .models import Genres
-from .models import Bookratings
-admin.site.register(Wishlists)
-admin.site.register(Books)
-admin.site.register(Genres)
-admin.site.register(Bookratings)
