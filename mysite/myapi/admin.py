f7e71d04c2b999c8ff66e87e5217399bbf4fd8c7
carlosmoya002 <83508169+carlosmoya002@users.noreply.github.com>
2022-10-12 19:35:01 -0400

    Update admin.py

--- mysite/myapi/admin.py
+++ mysite/myapi/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+
 from .models import Wishlists
 from .models import Books
 from .models import Genres
 from .models import Bookratings
+from .models import Purchasedbooks
+
 admin.site.register(Wishlists)
 admin.site.register(Books)
 admin.site.register(Genres)
 admin.site.register(Bookratings)
+admin.site.register(Purchasedbooks)
